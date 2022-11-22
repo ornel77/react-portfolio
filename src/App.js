2253b21d575eb38da699647b78ce25e0d0ba36f3
ornel77 <t.ornel@yahoo.fr>
2022-11-22 14:22:01 +0100

    landing done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+
+import Navbar from "../src/scenes/Navbar";
+import DotGroup from "./scenes/DotGroup";
+import Landing from "./scenes/Landing";
 
 
 function App() {
+  const [selectedPage, setSeletedPage] = useState('home') // determines what page we are on
+  const [isTopOfPage, setIsTopOfPage] = useState(true)
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)") // check if the user browser is above or less than 1600px
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY === 0) setIsTopOfPage(true)
+      if(window.scrollY !== 0) setIsTopOfPage(false)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <div className="App">
+    <div className="app bg-deep-blue">
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSeletedPage={setSeletedPage} />
+      <div className="w-5/6 mx-auto md:h-full">
+        { isAboveMediumScreens && (
+          <DotGroup selectedPage={selectedPage} setSeletedPage={setSeletedPage} />
+        )}
+        <Landing setSeletedPage={setSeletedPage} />
+      </div>
     </div>
   );
 }
--- src/hooks/useMediaQuery.jsx
+++ src/hooks/useMediaQuery.jsx
@@ -1,21 +1,21 @@
-/* ------------- check if you match a breakpoint of a media size ------------ */
+
+
 import { useState, useEffect } from "react";
 
-const useMediaQuery = query => {
-    const [matches, setMatches] = useState(false);
+const useMediaQuery = (query) => {
+  const [matches, setMatches] = useState(false);
 
-    useEffect(() => {
-        const media = window.matchesMedia(query)
-        if(media.matches !== matches) {
-            setMatches(media.matches);
-        }
-        const listener = () => setMatches(media.matches);
-        window.addEventListener('resize', listener);
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    window.addEventListener("resize", listener);
+    return () => window.removeEventListener("resize", listener);
+  }, [matches, query]);
 
-        return () => window.removeEventListener('resize', listener)
-    }, [matches, query])
+  return matches;
+};
 
-    return matches
-}
-
-export default useMediaQuery
+export default useMediaQuery;
