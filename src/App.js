04711650d2220f54aab0ee6e5b3f2075206edf53
ornel77 <t.ornel@yahoo.fr>
2022-11-23 15:50:07 +0100

    portfolio done

--- src/App.js
+++ src/App.js
@@ -6,34 +6,39 @@ import DotGroup from "./scenes/DotGroup";
 import Landing from "./scenes/Landing";
 import MySkills from "./scenes/MySkills";
 import Projects from "./scenes/Projects";
+import Testimonials from "./scenes/Testimonials";
+import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
 
 import LineGradient from "./components/LineGradient";
 
 
 function App() {
-  const [selectedPage, setSeletedPage] = useState('home') // determines what page we are on
+  const [selectedPage, setSelectedPage] = useState('home') // determines what page we are on
   const [isTopOfPage, setIsTopOfPage] = useState(true)
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)") // check if the user browser is above or less than 1600px
 
 
   useEffect(() => {
     const handleScroll = () => {
-      if(window.scrollY === 0) setIsTopOfPage(true)
-      if(window.scrollY !== 0) setIsTopOfPage(false)
-    }
-
-    window.addEventListener("scroll", handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="app bg-deep-blue">
-      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSeletedPage={setSeletedPage} />
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
       <div className="w-5/6 mx-auto md:h-full">
         { isAboveMediumScreens && (
-          <DotGroup selectedPage={selectedPage} setSeletedPage={setSeletedPage} />
+          <DotGroup selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
         )}
-        <Landing setSeletedPage={setSeletedPage} />
+        <Landing setSelectedPage={setSelectedPage} />
       </div>
       <LineGradient/>
       <div className="w-5/6 mx-auto md:h-full">
@@ -43,6 +48,15 @@ function App() {
       <div className="w-5/6 mx-auto">
           <Projects/>
       </div>
+      <LineGradient/>
+      <div className="w-5/6 mx-auto md:h-full">
+          <Testimonials/>
+      </div>
+      <LineGradient/>
+      <div className="w-5/6 mx-auto md:h-full">
+          <Contact/>
+      </div>
+      <Footer/>
     </div>
   );
 }
